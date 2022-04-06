c258921c346ee139c8b4f282e86409a9a5696c5f
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2022-04-05 22:21:56 -0400

    added pruebas automaticas con jest

---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+    id: "1",
+    title: "Coffe Mug - Sin Imagen",
+    // img: "./images/coffee-mug.png",
+  };
+
+  export const product2 = {
+    id: "2",
+    title: "Coffe Mug - Card - Con Imagen",
+    img: "./images/coffee-mug.png"
+  };
